859bed98c63181b73be318d3986617d9c93b5fb4
Salmaliakhat <82182503+Salmaliakhat@users.noreply.github.com>
2021-04-10 20:53:43 +0530

    Add files via upload

---
+++ DayMinTempSpread/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace DayMinTempSpread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var enviroment = System.Environment.CurrentDirectory;
+            string projectDirectory = Directory.GetParent(enviroment).Parent.FullName;
+
+            string rawData = File.ReadAllText(projectDirectory+"/App_Data/weather.dat");
+
+            var temperatureData = TemperatureData.ExtractFromRawData(rawData);
+            Console.WriteLine($"Read {temperatureData.Count()} lines of weather data.");
+
+            Func<TemperatureData, int> temperatureSpread = data => data.MaxTemp - data.MinTemp;
+            var smallestSpread = temperatureData.OrderBy(temperatureSpread).First();
+
+            Console.WriteLine("Day with the smallest temperature spread:");
+            Console.WriteLine($"{smallestSpread.Day} (with a temperature spread of {temperatureSpread(smallestSpread)}°F)");
+
+            Console.ReadLine();
+        }
+    }
+}
